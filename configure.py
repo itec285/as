67d28eb7e7c2d53ea67b3010d9e746cd6435aab0
itec285 <i.am.mikelong@gmail.com>
2017-08-22 16:23:44 -0600

    Get Internal IP

--- configure.py
+++ configure.py
@@ -1,5 +1,18 @@
 import os, subprocess
 
+#For getting our IP address
+import socket, fcntl, struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+internal_IPAddress = get_ip_address('eth0')  # should return something like '192.168.0.110'
+
 #Ask the important questions, and then create the XML File
 from lxml import etree as ET
 
