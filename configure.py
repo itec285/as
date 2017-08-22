83af4e2a2cbb3698d8e4f628dc0b87c992c6fbb0
itec285 <i.am.mikelong@gmail.com>
2017-08-22 16:56:15 -0600

    Get external IP

--- configure.py
+++ configure.py
@@ -1,7 +1,7 @@
 import os, subprocess
 
-#For getting our IP address
-import socket, fcntl, struct
+#For getting our IP address - requests is external, everything else is internal
+import socket, fcntl, struct, requests
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,6 +14,8 @@ def get_ip_address(ifname):
 internalIPAddress = get_ip_address('eth0')  # should return something like '192.168.0.110'
 #print ('The internal IP address is: ' + internalIPAddress)
 
+externalIPAddress = requests.get('https://ipapi.co/ip/').text
+#print ('The external IP address is: ' + externalIPAddress)
 
 #Ask the important questions, and then create the XML File
 from lxml import etree as ET
@@ -31,6 +33,7 @@ doc = ET.SubElement(root, "doc")
 ET.SubElement(doc, "field1", name="storeCode").text = storeCode
 ET.SubElement(doc, "field2", name="tillNumber").text = tillNumber
 ET.SubElement(doc, "field3", name="internalIPAddress").text = internalIPAddress
+ET.SubElement(doc, "field4", name="externalIPAddress").text = externalIPAddress
 
 tree = ET.ElementTree(root)
 tree.write("filename.xml", pretty_print=True)
