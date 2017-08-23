16b6e257bf03c4389ab808156bb375a3b5b3f9fa
itec285 <i.am.mikelong@gmail.com>
2017-08-23 16:52:45 -0600

    Added the white splash screen

--- first-boot.sh
+++ first-boot.sh
@@ -3,4 +3,12 @@
 # This file is typically called from the first-boot.py file
 
 echo "First Boot" >> /home/pi/bootlog.txt
+
+#Move Desktop wallpaper in.  This is a bit of a hack right now, as we are just moving ours to the default name.
 cp /home/pi/as/road.jpg /usr/share/rpd-wallpaper/
+
+#Change the splash screen to ours
+cp /home/pi/as/splash.png /etc/splash.png
+cp /home/pi/as/asplashscreen /etc/init.d/asplashscreen
+chmod a+x /etc/init.d/asplashscreen
+insserv /etc/init.d/asplashscreen
