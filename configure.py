22ec9f4b7595514250bb941c71099f4c14ba2d97
itec285 <i.am.mikelong@gmail.com>
2017-09-11 00:30:12 -0600

    Changes to get RDP information from a REST webservice
    Significant changes to get JSON data from a REST webservice, given a store code, auth code, and till number.

--- configure.py
+++ configure.py
@@ -1,7 +1,9 @@
-import os, subprocess
+import os, subprocess, json, urllib
 
 #For getting our IP address - requests is external, everything else is internal
 import socket, fcntl, struct, requests
+
+serviceurl = 'http://localhost:5000/starplus/api/v1.0/rdplogin/'
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,32 +13,79 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
+
+def get_RDP_data(storeCode, authCode, tillNumber):
+	url = serviceurl + storeCode + '/' + authCode + '/' + tillNumber
+	#print ('Retrieving',url)
+	uh = urllib.urlopen(url)
+	data = uh.read()
+	#print ('Retrieved',len(data),'characters')
+	try: js = json.loads(str(data))
+	except: js = None
+	RDPData= js['data']
+	
+	#Below lines are for debugging only.
+	#print (json.dumps(js, indent=4))
+	#print ('--------------')
+	#print (js['data'])
+	#print type(RDPData)
+	#print len(RDPData)
+	#print ('RDP Address is:',RDPData[0]['RDPAddress'])
+	#print ('RDP Port is:',RDPData[0]['RDPPort'])
+	#print ('RDP Login is:',RDPData[0]['RDPLogin'])
+	#print ('RDP Password is:',RDPData[0]['RDPPassword'])
+	
+	return (RDPData)
+
 internalIPAddress = get_ip_address('eth0')  # should return something like '192.168.0.110'
 #print ('The internal IP address is: ' + internalIPAddress)
 
 externalIPAddress = requests.get('https://ipapi.co/ip/').text
 #print ('The external IP address is: ' + externalIPAddress)
 
-#Ask the important questions, and then create the XML File
-from lxml import etree as ET
-
+#Ask the important questions
 storeCode = raw_input('Enter the store code: ')
 #print (storeCode)
+
+authCode = raw_input('Enter your authorization number: ')
+#print (tillNumber)
 
 tillNumber = raw_input('Enter the till number: ')
 #print (tillNumber)
 
+#Get a dictionary of our RDP server address, port, login, and password given our storecode, authorization number (secret), and tillNumber
+RDPData = get_RDP_data(storeCode, authCode, tillNumber)
+
+#Below lines are for debugging only.
+#print ('RDP Address is:',RDPData[0]['RDPAddress'])
+#print ('RDP Port is:',RDPData[0]['RDPPort'])
+#print ('RDP Login is:',RDPData[0]['RDPLogin'])
+#print ('RDP Password is:',RDPData[0]['RDPPassword'])
+
+RDPAddress = RDPData[0]['RDPAddress']
+RDPPort = str(RDPData[0]['RDPPort'])
+RDPLogin = RDPData[0]['RDPLogin']
+RDPPassword = RDPData[0]['RDPPassword']
+
 #Write the results to an XML file
+#Import the needed module
+from lxml import etree as ET
+
 root = ET.Element("root")
 doc = ET.SubElement(root, "doc")
 
 ET.SubElement(doc, "field1", name="storeCode").text = storeCode
-ET.SubElement(doc, "field2", name="tillNumber").text = tillNumber
-ET.SubElement(doc, "field3", name="internalIPAddress").text = internalIPAddress
-ET.SubElement(doc, "field4", name="externalIPAddress").text = externalIPAddress
+ET.SubElement(doc, "field1", name="authCode").text = authCode
+ET.SubElement(doc, "field3", name="tillNumber").text = tillNumber
+ET.SubElement(doc, "field4", name="internalIPAddress").text = internalIPAddress
+ET.SubElement(doc, "field5", name="externalIPAddress").text = externalIPAddress
+ET.SubElement(doc, "field6", name="RDPAddress").text = RDPAddress
+ET.SubElement(doc, "field7", name="RDPPort").text = RDPPort
+ET.SubElement(doc, "field8", name="RDPLogin").text = RDPLogin
+ET.SubElement(doc, "field9", name="RDPPassword").text = RDPPassword
 
 tree = ET.ElementTree(root)
-tree.write("filename.xml", pretty_print=True)
+tree.write("configuration.xml", pretty_print=True)
 
 #Prepare the system to run till2
 print ('SAY YES')
