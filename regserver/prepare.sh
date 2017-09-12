cb88a9eb1b38744d1b92cbe2386627c0878d2025
itec285 <i.am.mikelong@gmail.com>
2017-09-12 09:14:16 -0600

    Update and rename hist to prepare.sh

--- regserver/prepare.sh
+++ regserver/prepare.sh
@@ -1,11 +1,14 @@
-mkdir regserver
+#!/bin/sh
+mkdir ~/regserver
+cd ~/regserver
 sudo apt-get install python3-venv
+sudo apt-get install sqlite3
 python3 -m venv rest-api
 rest-api/bin/pip install wheel
 rest-api/bin/pip install flask
 rest-api/bin/pip install flask-restful
 rest-api/bin/pip install flask-sqlalchemy
-cp /root/as/regserver/app.py /root/regserver/
-cp /root/as/regserver/starplus.db /root/regserver/
+cp ~/as/regserver/app.py ~/regserver/
+cp ~/as/regserver/starplus.db ~/regserver/
+cp ~/as/regserver/commands ~/regserver/
 chmod a+x app.py
-apt-get install sqlite3
