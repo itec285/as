4f84af1641082d4ec1a60916c101e273a9c44334
itec285 <i.am.mikelong@gmail.com>
2017-09-14 16:22:43 -0600

    Make login file executable

--- first-boot.py
+++ first-boot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os, subprocess
 
+loginFile='/home/pi/login.sh'
+
 #Define a function to change part of a file
 def inplace_change(filename, old_string, new_string):
     # Safely read the input filename using 'with'
@@ -16,16 +18,22 @@ def inplace_change(filename, old_string, new_string):
         s = s.replace(old_string, new_string)
         f.write(s)
 
-def create_login_file:
-	f = open("/home/pi/login.sh","w")
+def create_login_file(path):
+	f = open(path,"w")
 	f.write('#!/bin/sh' + '\n')
 	f.write('lxterminal --command=setup' + '\n')
 	f.close()
+
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
         
 #Use the inplace change function to replace British (GB) keymapping with US Keyboard
 inplace_change('/etc/default/keyboard','gb','us')
 
-create_login_file()
+create_login_file(loginFile)
+make_executable(loginFile)
 
 #Call the first boot shell script
 subprocess.call(['home/pi/as/first-boot.sh'])
