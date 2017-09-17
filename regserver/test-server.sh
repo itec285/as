a9670dc0168397109c41a1318aa80593c9efe4f8
itec285 <i.am.mikelong@gmail.com>
2017-09-17 01:44:37 -0600

    Added command to test POST.

--- regserver/test-server.sh
+++ regserver/test-server.sh
@@ -2,3 +2,7 @@ curl -i http://localhost:5000/starplus/api/v1.0/storecodes
 curl -i http://localhost:5000/starplus/api/v1.0/rdplogin/TEST01/AAA1/2
 curl -i http://159.203.41.250:5000/starplus/api/v1.0/storecodes
 curl -i http://159.203.41.250:5000/starplus/api/v1.0/rdplogin/TEST01/AAA1/2
+
+#Testing the POST command
+curl -i -H "Content-Type application/json" -X POST http://localhost:5000/starplus/api/v1.0/register/TEST01/2/216.123.248.66/192.168.0.12
+
