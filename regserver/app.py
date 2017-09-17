12b3daa72a74e4afb5db3ec3d90ee9aed2131ab5
itec285 <i.am.mikelong@gmail.com>
2017-09-17 01:39:56 -0600

    Added the register resource.
    Uses the new registration table.  Test with:
    
    # curl -i -H "Content-Type application/json" -X POST http://localhost:5000/starplus/api/v1.0/register/TEST01/2/216.123.248.66/192.168.0.12

--- regserver/app.py
+++ regserver/app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify#, HTTPBasicAuth
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+import datetime
 
 #Create a engine for connecting to SQLite3
 #Assuming starplus.db is in your app root folder
@@ -26,16 +27,28 @@ class StoreCodes_Meta(Resource):
 class RDP_Login(Resource):
 	def get(self, store_code, auth_code, till_number):
 		conn = e.connect()
-		query = conn.execute("select * from RDPLogins where STORECODE=? and AUTHCODE = ? and TILLNUMBER = ?",(store_code.upper(), auth_code, till_number))
+		query = conn.execute("SELECT * FROM RDPLogins WHERE STORECODE=? AND AUTHCODE = ? and TILLNUMBER = ?",(store_code.upper(), auth_code, till_number))
 		#Query the result and get cursor.  
 		result = {'data': [dict(zip(tuple (query.keys()) ,auth_code)) for auth_code in query.cursor]}
 		#To debug result try  >return len(str(result)) or just >return str(result)
 		#return jsonify(result)
 		return result
-		
 
-api.add_resource(RDP_Login, '/starplus/api/v1.0/rdplogin/<string:store_code>/<string:auth_code>/<string:till_number>')
+class Register(Resource):
+	def post(self, store_code, till_number, external_IPAddress, internal_IPAddress):
+		now = datetime.datetime.now()
+		conn = e.connect()
+		query = conn.execute("INSERT INTO Registration(DATE, STORECODE, TILLNUMBER, EXTERNALIPADDRESS, INTERNALIPADDRESS ) VALUES(?,?,?,?,?)", (now, store_code.upper(), till_number, external_IPAddress, internal_IPAddress))
+		
+		#Old way 
+		#return query.lastrowid
+		
+		result = {'data':query.lastrowid}
+		return jsonify(result)
+
 api.add_resource(StoreCodes_Meta, '/starplus/api/v1.0/storecodes')
+api.add_resource(RDP_Login, '/starplus/api/v1.0/rdplogin/<string:store_code>/<string:auth_code>/<string:till_number>')
+api.add_resource(Register, '/starplus/api/v1.0/register/<string:store_code>/<int:till_number>/<string:external_IPAddress>/<string:internal_IPAddress>')
 
 if __name__ == '__main__':
 #	app.run()
