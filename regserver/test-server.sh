b7e718372e5d0db7eb22c25ca41e548c36f9ac4d
itec285 <i.am.mikelong@gmail.com>
2017-09-17 21:31:27 -0600

    Changed test on the register command from POST to GET

--- regserver/test-server.sh
+++ regserver/test-server.sh
@@ -3,6 +3,9 @@ curl -i http://localhost:5000/starplus/api/v1.0/rdplogin/TEST01/AAA1/2
 curl -i http://159.203.41.250:5000/starplus/api/v1.0/storecodes
 curl -i http://159.203.41.250:5000/starplus/api/v1.0/rdplogin/TEST01/AAA1/2
 
-#Testing the POST command
+#OLD WAYTesting the POST command
 curl -i -H "Content-Type application/json" -X POST http://localhost:5000/starplus/api/v1.0/register/TEST01/2/216.123.248.66/192.168.0.12
+
+#Testing the register via a get command
+curl -i -H "Content-Type application/json" http://localhost:5000/starplus/api/v1.0/register/TEST01/2/216.123.248.66/192.168.0.12
 
